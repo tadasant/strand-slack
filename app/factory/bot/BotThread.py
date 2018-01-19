c6159bdc21cf653eb625c33be2b6b505e3436eea
William Fry <wrfry95@gmail.com>
2018-01-19 15:19:21 -0500

    Refactor to pause / resume. Only use stop to destroy.

--- app/factory/bot/BotThread.py
+++ app/factory/bot/BotThread.py
@@ -6,17 +6,29 @@ class BotThread(threading.Thread):
     def __init__(self):
         super(BotThread, self).__init__()
         self._stop_event = threading.Event()
+        self._pause_event = threading.Event()
         self.daemon = True
+
+    @property
+    def is_paused(self):
+        return self._pause_event.is_set()
 
     @property
     def is_stopped(self):
         return self._stop_event.is_set()
 
+    def pause(self):
+        self._pause_event.set()
+
+    def resume(self):
+        self._pause_event.clear()
+
     def run(self):
         self._stop_event.clear()
         while not self._stop_event.is_set():
-            time.sleep(1)
-            print(self.name)
+            while not self._pause_event.is_set():
+                time.sleep(1)
+                print(self.name)
 
     def stop(self):
         self._stop_event.set()
