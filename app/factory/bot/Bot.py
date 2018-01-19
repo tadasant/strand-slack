7e99ea5535045c94507a642b2a355079973145d9
William Fry <wrfry95@gmail.com>
2018-01-19 08:30:11 -0500

    Implemented Factory with sample Bot classes.

---
+++ app/factory/bot/Bot.py
@@ -0,0 +1,44 @@
+from app import app
+from app.factory.bot.BotThread import BotThread
+
+
+class Bot:
+    def __init__(self, slack_team_name, slack_team_id, access_token, installer_id, bot_user_id, bot_access_token):
+        self.slack_team_name = slack_team_name
+        self.slack_team_id = slack_team_id
+        self.access_token = access_token
+        self.installer_id = installer_id
+        self.bot_user_id = bot_user_id
+        self.bot_access_token = bot_access_token
+        self.bot_thread = None
+
+    def start(self):
+        if not self.bot_thread:
+            self.bot_thread = BotThread()
+            app.logger.debug(f'Bot in team "{self.slack_team_name}" started')
+        else:
+            if self.bot_thread.is_stopped:
+                self.bot_thread.run()
+                app.logger.debug(f'Bot in team "{self.slack_team_name}" started')
+            else:
+                app.logger.debug(f'Bot in team "{self.slack_team_name}" already started')
+        return True
+
+    def is_alive(self):
+        return self.bot_thread and self.bot_thread.is_alive()
+
+    def stop(self):
+        if self.bot_thread and not self.bot_thread.is_stopped:
+            self.bot_thread.stop()
+            app.logger.debug(f'Bot in team "{self.slack_team_name}" stopped')
+        else:
+            app.logger.debug(f'Bot in team {self.slack_team_name} already stopped')
+
+    def destroy(self):
+        if self.bot_thread:
+            if self.bot_thread.is_stopped:
+                self.bot_thread.join()
+            else:
+                self.bot_thread.stop()
+                self.bot_thread.join()
+        app.logger.debug(f'Bot in team "{self.slack_team_name}" destroyed')
--- app/factory/bot/BotThread.py
+++ app/factory/bot/BotThread.py
@@ -4,11 +4,10 @@ import time
 from app import app
 
 
-class BotThread(threading.Thread):
-    def __init__(self, name):
-        super(BotThread, self).__init__()
+class EventMonitorThread(threading.Thread):
+    def __init__(self):
+        super(EventMonitorThread, self).__init__()
         self._stop_event = threading.Event()
-        self.bot_name = name
         self.daemon = True
 
     @property
@@ -19,7 +18,7 @@ class BotThread(threading.Thread):
         self._stop_event.clear()
         while not self._stop_event.is_set():
             time.sleep(1)
-            app.logger.info(self.name)
+            app.logger.debug(self.name)
 
     def stop(self):
         self._stop_event.set()
