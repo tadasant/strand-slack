59cfd27562e977116c192b139a954343e5a6c12c
William Fry <wrfry95@gmail.com>
2018-01-19 21:32:53 -0500

    Update test_create_bot

--- tests/func/test_factory_blueprint.py
+++ tests/func/test_factory_blueprint.py
@@ -1,8 +1,13 @@
+import threading
+
+
 class TestFactoryBlueprint:
     def test_get_bots(self, client):
         res = client.get('/factory/bots')
         assert res.json == []
 
     def test_create_bot(self, client):
+        assert threading.active_count() == 1
         res = client.post('/factory/bots')
-        assert res.json == {}
+        assert res.json == {'created': True}
+        assert threading.active_count() == 2
