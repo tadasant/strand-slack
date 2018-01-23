98edac521d11b9ce6cc785c602d4fec6ce95b049
Tadas Antanavicius <tadas@solutionloft.com>
2018-01-23 12:02:05 -0500

    clarifying comments on wrapper

--- src/wrappers/SlackClientWrapper.py
+++ src/wrappers/SlackClientWrapper.py
@@ -4,16 +4,20 @@ from src.common.logging import get_logger
 
 
 class SlackClientWrapper:
+    """Manages all outgoing interaction with Slack APIs"""
+
     def __init__(self, tokens_by_team_id, log_file):
         self.tokens_by_team_id = tokens_by_team_id if tokens_by_team_id else {}
         self.logger = get_logger('SlackClientWrapper', log_file)
 
-    def add_tokens(self, tokens, team_id):
+    def set_tokens(self, tokens, team_id):
+        """Register tokens for a new team that the wrapper can leverage (new or updated)"""
         self.tokens_by_team_id[team_id] = tokens
 
     def do_some_action(self, team_id):
         """Illustrative template (remove later)"""
         if team_id not in self.tokens_by_team_id:
             raise Exception  # TODO make a custom error type
+        # TODO retry logic
         client = SlackClient(token=self.tokens_by_team_id[team_id].bot_access_token)
         # client.api_call....
