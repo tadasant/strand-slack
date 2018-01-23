fa8346c015656bb6aa5b3cf99221153f8ef63694
Tadas Antanavicius <tadas@solutionloft.com>
2018-01-23 11:52:56 -0500

    slackclientwrapper in memory tokens set up

--- src/wrappers/PortalClientWrapper.py
+++ src/wrappers/PortalClientWrapper.py
@@ -12,12 +12,13 @@ class PortalClientWrapper:
         self.portal_client = PortalClient(host=host, endpoint=endpoint)
         self.logger = get_logger('PortalClientWrapper', log_file)
 
-    def get_slack_tokens(self):
+    def get_slack_tokens_by_slack_team_id(self):
         operation_definition = '''
             {
                 slackTeamInstallations {
                     botAccessToken
                     accessToken
+                    slackTeam
                 }
             }
         '''
@@ -35,6 +36,7 @@ class PortalClientWrapper:
         except PortalClientException as e:
             self.logger.error(f'Failed when calling PortalClient. Error: {e}')
             raise WrapperException(wrapper_name='PortalClient', message='Failed when calling PortalClient')
-        return [SlackTokens(bot_access_token=x['botAccessToken'],
-                            access_token=x['accessToken'])
-                for x in slack_installations]
+        return {
+            x['slackTeam']: SlackTokens(bot_access_token=x['botAccessToken'], access_token=x['accessToken'])
+            for x in slack_installations
+        }
--- src/wrappers/SlackClientWrapper.py
+++ src/wrappers/SlackClientWrapper.py
@@ -4,7 +4,11 @@ from src.common.logging import get_logger
 
 
 class SlackClientWrapper:
-    def __init__(self, bot_token, oauth_token, log_file):
-        self.bot_user_client = SlackClient(bot_token)
-        self.app_client = SlackClient(oauth_token)
+    def __init__(self, tokens_by_team_id, log_file):
+        self.tokens_by_team_id = tokens_by_team_id
         self.logger = get_logger('SlackClientWrapper', log_file)
+
+    def do_some_action(self, team_id):
+        """Illustrative template (remove later)"""
+        client = SlackClient(token=self.tokens_by_team_id[team_id].bot_access_token)
+        # client.api_call....
