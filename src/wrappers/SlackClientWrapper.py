db51b47d1a783d0ba7da415472596e1e227d80be
Tadas Antanavicius <tadas@solutionloft.com>
2018-01-23 11:59:21 -0500

    exposed adding tokens

--- src/wrappers/SlackClientWrapper.py
+++ src/wrappers/SlackClientWrapper.py
@@ -5,10 +5,15 @@ from src.common.logging import get_logger
 
 class SlackClientWrapper:
     def __init__(self, tokens_by_team_id, log_file):
-        self.tokens_by_team_id = tokens_by_team_id
+        self.tokens_by_team_id = tokens_by_team_id if tokens_by_team_id else {}
         self.logger = get_logger('SlackClientWrapper', log_file)
+
+    def add_tokens(self, tokens, team_id):
+        self.tokens_by_team_id[team_id] = tokens
 
     def do_some_action(self, team_id):
         """Illustrative template (remove later)"""
+        if team_id not in self.tokens_by_team_id:
+            raise Exception  # TODO make a custom error type
         client = SlackClient(token=self.tokens_by_team_id[team_id].bot_access_token)
         # client.api_call....
