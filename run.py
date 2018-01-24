e616a3fce1b0a5e05c0e52953ba23b5a68e4f774
Tadas Antanavicius <tadas@solutionloft.com>
2018-01-24 09:44:56 -0500

    clean up some config

--- run.py
+++ run.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
         portal_client=PortalClient(host=config['PORTAL_HOST'], endpoint=config['PORTAL_GRAPHQL_ENDPOINT']),
         SlackClientClass=SlackClient
     )
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=config['FLASK_DEBUG'], host='0.0.0.0')
