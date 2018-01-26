6563ce1f41a2bea6c0882430ab35a37765497a77
Tadas Antanavicius <tadas@solutionloft.com>
2018-01-25 20:49:53 -0500

    added retry logic

--- src/command/messages/onboarding_dm.py
+++ src/command/messages/onboarding_dm.py
@@ -5,7 +5,7 @@ OnboardingDmType = namedtuple('OnboardingDmType', 'callback_id text attachment')
 _onboarding_dm_callback_id = 'onboarding_dm'
 ONBOARDING_DM = OnboardingDmType(
     callback_id=_onboarding_dm_callback_id,
-    text="Hey there! I'm your CodeClippy agent :sleuth_or_spy:\n\nWhat channel should I use for" \
+    text="Hey there! I'm your CodeClippy agent :sleuth_or_spy:\n\nWhat channel should I use for "
          "showing help requests?",
     attachment={
         "fallback": "Upgrade your Slack client to use messages like these.",
--- src/wrappers/SlackClientWrapper.py
+++ src/wrappers/SlackClientWrapper.py
@@ -1,6 +1,7 @@
+from tenacity import Retrying, wait_fixed, stop_after_attempt, after_log, retry_if_exception_type, retry_if_result
+
 from src import slack_agent_repository
 from src.common.logging import get_logger
-from src.domain.models.exceptions.WrapperException import WrapperException
 
 
 class SlackClientWrapper:
@@ -9,16 +10,26 @@ class SlackClientWrapper:
     def __init__(self, SlackClientClass):
         self.SlackClientClass = SlackClientClass
         self.logger = get_logger('SlackClientWrapper')
+        self.standard_retrier = Retrying(
+            reraise=True,
+            wait=wait_fixed(2),
+            stop=stop_after_attempt(5),
+            after=after_log(logger=self.logger, log_level=self.logger.getEffectiveLevel()),
+            retry=(retry_if_exception_type(ConnectionError) | retry_if_result(self._is_response_not_ok))
+        )
 
     def _get_slack_client(self, slack_team_id, is_bot=False):
         repo = slack_agent_repository
         token = repo.get_slack_bot_access_token(slack_team_id=slack_team_id) if is_bot else repo.get_slack_access_token(
             slack_team_id=slack_team_id)
-        # TODO retry logic
         return self.SlackClientClass(token=token)
+
+    def _is_response_not_ok(self, response):
+        return not response['ok']
 
     def send_dm_to_user(self, slack_team_id, slack_user_id, text, attachments=[]):
         slack_client = self._get_slack_client(slack_team_id=slack_team_id, is_bot=True)
-        # TODO retry logic
-        slack_channel_id = slack_client.api_call('im.open', user=slack_user_id)['channel']['id']
-        slack_client.api_call('chat.postMessage', channel=slack_channel_id, text=text, attachments=attachments)
+        response = self.standard_retrier.call(slack_client.api_call, method='im.open', user=slack_user_id)
+        slack_channel_id = response['channel']['id']
+        self.standard_retrier.call(slack_client.api_call, method='chat.postMessage', channel=slack_channel_id,
+                                   text=text, attachments=attachments)
