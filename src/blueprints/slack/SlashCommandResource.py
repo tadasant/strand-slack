fb59f6d577066c330345ce59ef85df34bfe7ac93
Tadas Antanavicius <tadas@solutionloft.com>
2018-01-29 16:08:35 -0500

    exposing slash resource

--- src/blueprints/slack/SlashCommandResource.py
+++ src/blueprints/slack/SlashCommandResource.py
@@ -24,6 +24,7 @@ class SlashCommandResource(SlackResource):
                                                      slack_user_id=r.user_id)
             Thread(target=command.execute).start()
         else:
+            # TODO send user an ephemeral help message instead of silently failing here
             message = f'Could not interpret slack request: {r}'
             self.logger.error(message)
             raise UnexpectedSlackException(message=message)
