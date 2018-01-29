5464b7245c2c32be746cbc3d6645ceb13329cafe
Tadas Antanavicius <tadas@solutionloft.com>
2018-01-29 11:26:19 -0500

    added thread and fixed test

--- src/blueprints/slack/InteractiveComponentResource.py
+++ src/blueprints/slack/InteractiveComponentResource.py
@@ -1,4 +1,6 @@
 import json
+from http import HTTPStatus
+from threading import Thread
 
 from flask import current_app, request
 from flask_restful import Resource
@@ -28,15 +30,15 @@ class InteractiveComponentResource(Resource):
         interactive_menu_response = InteractiveMenuResponseSchema().load(payload).data
         r = interactive_menu_response
         if r.is_help_channel_selection:
-            # TODO [CCS-38] multithread commands -- should respond to Slack immediately
-            response = UpdateHelpChannelCommand(slack_client_wrapper=current_app.slack_client_wrapper,
-                                                portal_client_wrapper=current_app.portal_client_wrapper,
-                                                slack_team_id=r.team.id,
-                                                help_channel_id=r.selected_help_channel_id,
-                                                response_url=r.response_url).execute()
+            command = UpdateHelpChannelCommand(slack_client_wrapper=current_app.slack_client_wrapper,
+                                               portal_client_wrapper=current_app.portal_client_wrapper,
+                                               slack_team_id=r.team.id,
+                                               help_channel_id=r.selected_help_channel_id,
+                                               response_url=r.response_url)
+            Thread(target=command.execute).start()
         else:
             message = f'Could not interpret slack request: {r}'
             self.logger.error(message)
             raise UnexpectedSlackException(message=message)
 
-        return response, 200
+        return '', HTTPStatus.NO_CONTENT
