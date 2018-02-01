65084066f1fedd310baeb9e50314bfa3b482b02b
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-01 10:15:32 -0500

    stubbed out to command level on event resource

--- src/blueprints/slack/EventResource.py
+++ src/blueprints/slack/EventResource.py
@@ -4,6 +4,7 @@ from flask import request
 
 from src.blueprints.slack.SlackResource import SlackResource
 from src.domain.models.slack.requests.EventRequest import EventRequestSchema
+from src.domain.repositories.SlackAgentRepository import slack_agent_repository
 
 
 class EventResource(SlackResource):
@@ -16,6 +17,16 @@ class EventResource(SlackResource):
         if event_request.is_verification_request:
             return event_request.challenge, HTTPStatus.OK
         elif event_request.event and event_request.event.is_message_channels_event:
-            #  determine if it's relevant
-            pass
+            discuss_channel_id = slack_agent_repository.get_discuss_channel_id(slack_team_id=event_request.team_id)
+            if event_request.event.item.channel == discuss_channel_id:
+                self.logger.info('Detected message in #discuss channel')
+                # TODO [CCS-75] Command to delete non-clippy #discuss channel messages
+                # command = None
+                # Thread(target=command.execute, daemon=True).start()
+            else:
+                # TODO [CCS-81] Check whether or not this is #discussions-X vs. other should happen here via db
+                self.logger.info('Message in non-discuss channel')
+                # TODO [CCS-71] Command to forward messages to portal
+                # command = None
+                # Thread(target=command.execute, daemon=True).start()
         return {}, HTTPStatus.OK
