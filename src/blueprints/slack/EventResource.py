6049962eb9c7452940d408a438cf4a65019326bd
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-01 10:00:14 -0500

    skeleton for handling events

--- src/blueprints/slack/EventResource.py
+++ src/blueprints/slack/EventResource.py
@@ -1,13 +1,21 @@
+from http import HTTPStatus
+
 from flask import request
 
 from src.blueprints.slack.SlackResource import SlackResource
+from src.domain.models.slack.requests.EventRequest import EventRequestSchema
 
 
 class EventResource(SlackResource):
     def post(self):
         """Receiving an event from Slack"""
         self.logger.info(f'Processing Event request: {request}')
-        # type == 'url_verification'
-        # TODO handle channel messages as well -- do parsing of "discussions-X" after a channels.info query for now
-        # later we'll replace w/ querying our own DB to check
-        return request.json['challenge'], 200
+        payload = request.json
+        self._authenticate(payload)
+        event_request = EventRequestSchema().load(payload).data
+        if event_request.is_verification_request:
+            return event_request.challenge, HTTPStatus.OK
+        elif event_request.event and event_request.event.is_message_channels_event:
+            #  determine if it's relevant
+            pass
+        return {}, HTTPStatus.OK
