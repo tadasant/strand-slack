2d05454c75543cb60f68b8fd11f9d4e921b310a2
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-06 16:42:51 -0500

    updating discuss channel message

--- src/command/CloseDiscussionCommand.py
+++ src/command/CloseDiscussionCommand.py
@@ -1,12 +1,12 @@
 from src.command.Command import Command
 
 
-class CloseDiscussionOnPortalCommand(Command):
+class CloseDiscussionCommand(Command):
     def __init__(self, portal_client_wrapper, slack_channel_id, slack_team_id):
         super().__init__(portal_client_wrapper=portal_client_wrapper, slack_team_id=slack_team_id)
         self.slack_channel_id = slack_channel_id
 
     def execute(self):
-        log_msg = f'Executing CloseDiscussionOnPortalCommand for {self.slack_team_id} for chan {self.slack_channel_id}'
+        log_msg = f'Executing CloseDiscussionCommand for {self.slack_team_id} for chan {self.slack_channel_id}'
         self.logger.info(log_msg)
         self.portal_client_wrapper.close_discussion(slack_channel_id=self.slack_channel_id)
