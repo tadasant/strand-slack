0c4eb5ff3b4126327b7849c482ce69818426d456
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-08 16:05:05 -0500

    dont dm bot

--- src/command/InformTopicChannelMessageDeletedCommand.py
+++ src/command/InformTopicChannelMessageDeletedCommand.py
@@ -1,5 +1,6 @@
 from src.command.Command import Command
 from src.command.model.message.formatted_text import block_topic_channel_message
+from src.domain.models.portal.SlackUser import SlackUserSchema
 
 
 class InformTopicChannelMessageDeletedCommand(Command):
@@ -12,6 +13,12 @@ class InformTopicChannelMessageDeletedCommand(Command):
         log_msg = f'Executing InformTopicChannelMessageDeletedCommand for {self.slack_team_id} with user ' \
                   '{self.slack_user_id}'
         self.logger.info(log_msg)
-        dm_text = block_topic_channel_message(attempted_message=self.attempted_text)
-        self.slack_client_wrapper.send_dm_to_user(slack_team_id=self.slack_team_id, slack_user_id=self.slack_user_id,
-                                                  text=dm_text)
+        user_info = self.slack_client_wrapper.get_user_info(slack_team_id=self.slack_team_id,
+                                                            slack_user_id=self.slack_user_id)
+        slack_user = SlackUserSchema().load(user_info).data
+        if not slack_user.is_bot:
+            self.logger.info('Poster was not a bot, so informing them of their deleted message')
+            dm_text = block_topic_channel_message(attempted_message=self.attempted_text)
+            self.slack_client_wrapper.send_dm_to_user(slack_team_id=self.slack_team_id,
+                                                      slack_user_id=self.slack_user_id,
+                                                      text=dm_text)
