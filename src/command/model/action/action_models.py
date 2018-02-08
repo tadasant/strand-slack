6c079007a8b6e9d69b247b745daa1181d4a9a10a
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-08 14:06:35 -0500

    added option for confirmation

--- src/command/model/action/action_models.py
+++ src/command/model/action/action_models.py
@@ -1,15 +1,22 @@
 from src.command.model.action.Action import Action
+from src.command.model.action.Confirm import Confirm
 
 
 class PostNewTopicButton(Action):
     def __init__(self, require_confirmation=False):
-        # confirm = Confirm() if require_confirmation else None
+        confirm = Confirm(
+            title='Are you sure?',
+            text='This will start a new discussion',
+            ok_text='Yes',
+            dismiss_text='No'
+        ) if require_confirmation else None
         super().__init__(
             name='post',
             text='Post new topic',
             style='primary',
             type='button',
-            value='post'
+            value='post',
+            confirm=confirm
         )
 
 
