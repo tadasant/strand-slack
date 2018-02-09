d98bb98f1ec7e41c5bcf7f2c41805ef538c93dfa
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-09 10:25:20 -0500

    factored out portalresource

---
+++ src/blueprints/portal/PortalResource.py
@@ -0,0 +1,25 @@
+import re
+
+from flask import current_app, request
+from flask_restful import Resource
+
+from src.domain.models.exceptions.UnauthorizedException import UnauthorizedException
+
+
+class PortalResource(Resource):
+
+    @classmethod
+    def authenticate(cls, func):
+        token_regex = r'Token (.*)'
+
+        def wrapper(*args, **kwargs):
+            authorization_header = request.headers.get('Authorization')
+            if authorization_header:
+                matches = re.findall(token_regex, authorization_header)
+                if len(matches) == 1:
+                    token = matches[0]
+                    if token == current_app.portal_verification_token:
+                        return func(*args, **kwargs)
+            raise UnauthorizedException(message='Portal authorization failed.')
+
+        return wrapper
