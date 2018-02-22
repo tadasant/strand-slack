79a2d7bab41fb78c15d82211522800e89fdd9661
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-22 11:41:23 -0500

    incremented version number

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='strand-slack',
+      version='0.1.0',
+      description='Strand Slack Integration',
+      author='Strand',
+      author_email='hello@trystrand.com',
+      url='https://trystrand.com',
+      packages=['distutils', 'distutils.command'],
+      )
