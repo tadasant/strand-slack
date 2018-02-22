437d723763e8eeb323025b9c44ceeb5742122254
Tadas Antanavicius <tadas@solutionloft.com>
2018-02-22 11:01:01 -0500

    squashed newline bug

--- tests/unit/test_portal_client_wrapper.py
+++ tests/unit/test_portal_client_wrapper.py
@@ -35,3 +35,34 @@ def test_handles_double_quotes_in_new_topic(portal_client, mocker):
         }
       }
     ''')
+
+
+def test_handles_newlines_in_new_topic(portal_client, mocker):
+    mocker.spy(portal_client, 'mutate')
+    portal_client_wrapper = PortalClientWrapper(portal_client=portal_client)
+    with pytest.raises(WrapperException):  # TODO not ideal; won't need when portal client queuing is removed
+        portal_client_wrapper.create_topic(
+            title='abc123',
+            description='abc234\nline2',
+            tag_names=['abc', '1234'],
+            original_poster_slack_user_id='someid',
+        )
+    assert dedent(portal_client.mutate.call_args[1]['operation_definition']) == dedent(r'''
+      {
+        createTopicFromSlack(input: {title: "abc123",
+                                      description: "abc234\nline2",
+                                      originalPosterSlackUserId: "someid",
+                                      tags: [{name: "abc"},{name: "1234"}]
+                                    })
+        {
+          topic {
+            id
+            title
+            description
+            tags {
+              name
+            }
+          }
+        }
+      }
+    ''')
