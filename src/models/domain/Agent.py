edd3e39400a27143f49688e44c0e6c096630cc80
Tadas Antanavicius <tadas@solutionloft.com>
2018-03-09 17:16:06 -0500

    added str lengths

--- src/models/domain/Agent.py
+++ src/models/domain/Agent.py
@@ -1,15 +1,16 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, String, Enum
 from sqlalchemy.orm import relationship
 
+from src.models.domain.AgentStatus import AgentStatus
 from src.utilities.database import Base
 
 
 class Agent(Base):
     __tablename__ = 'agent'
 
-    slack_team_id = Column(String, primary_key=True)
+    slack_team_id = Column(String, primary_key=True, length=16)
     strand_team_id = Column(BigInteger)
-    status = Column(String, nullable=False)
+    status = Column(Enum(AgentStatus), nullable=False)
 
     # 1 <--> 0..1
     bot = relationship('Bot', uselist=False, back_populates='agent')
--- src/models/domain/Bot.py
+++ src/models/domain/Bot.py
@@ -7,9 +7,9 @@ from src.utilities.database import Base
 class Bot(Base):
     __tablename__ = 'bot'
 
-    access_token = Column(String, nullable=False)
-    user_id = Column(String, nullable=False)
+    access_token = Column(String, nullable=False, length=128)
+    user_id = Column(String, nullable=False, length=16)
 
     # 0..1 <--> 1
-    agent_slack_team_id = Column(String, ForeignKey('agent.slack_team_id'), primary_key=True)
+    agent_slack_team_id = Column(String, ForeignKey('agent.slack_team_id'), primary_key=True, length=16)
     agent = relationship('Agent', back_populates='bot')
