7bc5e7efecd33725480cbf699d2c05b794f23b4a
Tadas Antanavicius <tadas@solutionloft.com>
2018-03-13 16:58:48 -0400

    finished test refactoring

--- tests/func/TestInstallFixtures.py
+++ tests/func/TestInstallFixtures.py
@@ -3,9 +3,6 @@ from typing import NamedTuple
 
 import pytest
 
-from src.models.domain.Agent import Agent, AgentStatus
-from src.models.domain.Installation import Installation
-from src.models.domain.User import User
 from tests.common.PrimitiveFaker import PrimitiveFaker
 from tests.testresources.TestSlackClient import SlackRepository, clear_slack_state
 from tests.testresources.TestStrandApiClient import StrandRepository, clear_strand_state
@@ -44,13 +41,8 @@ class TestInstallFixtures:
         # Plant fake response in Slack state
         SlackRepository['oauth_access_responses_by_code'][fake_code] = fake_slack_oauth_access_response
         # Plant existing objects in DB
-        agent = Agent(slack_team_id=f.team_id, strand_team_id=0, status=AgentStatus.ACTIVE.name)
-        fake_user_id = str(PrimitiveFaker('bban'))
-        user = User(slack_user_id=fake_user_id, strand_user_id=0, agent_slack_team_id=f.team_id)
-        installation = Installation(access_token=f.access_token, scope=f.scope, installer_slack_user_id=fake_user_id,
-                                    installer_agent_slack_team_id=f.team_id)
-        db_session.add_all([agent, user, installation])
-        db_session.commit()
+        insert_agent_user_installation(slack_team_id=f.team_id, access_token=f.access_token, scope=f.scope,
+                                       db_session=db_session)
 
         yield response(code=fake_code, slack_oauth_access_response=fake_slack_oauth_access_response)
         clear_slack_state(keys=['oauth_access_responses_by_code'])
