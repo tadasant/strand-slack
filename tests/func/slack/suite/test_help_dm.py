334559d6a31d39d27e82bee349dd9a247654b560
Tadas Antanavicius <tadas@solutionloft.com>
2018-03-14 09:26:02 -0400

    working test nonhelp dm

--- tests/func/slack/suite/test_help_dm.py
+++ tests/func/slack/suite/test_help_dm.py
@@ -7,7 +7,7 @@ from flask import url_for
 from src.models.domain.User import User
 from tests.factories.slackfactories import SlackEventFactory
 from tests.func.slack.TestHelpDmFixtures import TestHelpDmFixtures
-from tests.utils.asserting import wait_for_extra_threads_to_die
+from tests.utils.asserting import wait_for_extra_threads_to_die, assert_values_in_call_args_list
 
 
 @pytest.mark.usefixtures('app')
@@ -42,10 +42,35 @@ class TestHelpDm(TestHelpDmFixtures):
         assert slack_client_class.api_call.call_args[1]['method'] == 'chat.postEphemeral'
         assert 'installing the app' not in slack_client_class.api_call.call_args[1]['text']
 
-    def test_nonhelp_dm_installed_user(self, installed_user, client, slack_client_class, strand_api_client,
-                                       db_session, mocker):
+    def test_nonhelp_dm_installed_user(self, installed_user: User, slack_client_class, slack_event_request_factory,
+                                       baseline_thread_count, client, mocker):
         """Ensure we don't send a help message in 'non-help' DMs"""
-        pass
+        target_url = url_for(endpoint=self.target_endpoint)
+        fake_slack_event_request = slack_event_request_factory(
+            type='event_callback',
+            challenge=None,
+            team_id=installed_user.agent_slack_team_id,
+            event=SlackEventFactory.create(
+                user=installed_user.slack_user_id,
+                text='this is not a help dm',
+                channel='D12345678',
+                type='message'
+            )
+        )
+        payload = json.loads(fake_slack_event_request.to_json())
+        mocker.spy(slack_client_class, 'api_call')
+
+        response = client.post(path=target_url, headers=self.default_headers, data=json.dumps(payload))
+        assert wait_for_extra_threads_to_die(baseline_count=baseline_thread_count), 'Extra threads timed out'
+        assert HTTPStatus.OK == response.status_code
+
+        assert_values_in_call_args_list(
+            params_to_expecteds=[
+                {'method': 'chat.postEphemeral'},  # Ensure help message was not send
+            ],
+            call_args_list=slack_client_class.api_call.call_args_list,
+            expect_succeed=False
+        )
 
     def test_help_dm_new_user(self, client, slack_client_class, strand_api_client, db_session, mocker):
         """All users who do not have the app installed should recieve a help to install DM response"""
